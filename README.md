2d80e6e3c834de2eec3667201318e583533a1316
albshin <albshin07@gmail.com>
2017-07-12 12:13:15 -0400

    Add basic API wrapper functionality

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # go-pubg
+
+API wrapper for Playerunknown's Battlegrounds using the API from http://pubgtracker.com.
+
+## Getting Started
+
+### 1. Install the package
+
+Install the package and import it
+
+```
+go get github.com/albshin/go-pubg
+```
+
+### 2. Obtain API Key
+
+Create an account at https://pubgtracker.com/site-api and request an API Key.
+
+## Usage
+
+```
+client := pubg.CreateAPI("YOURAPIKEYHERE")
+info := client.GetPlayerByName("JohnDoe") // Returns JSON unfiltered for player "JohnDoe"
+```
+
+## To-Do
+* Documentation
+* Tests
+* Filtered results
---
+++ client.go
@@ -0,0 +1,139 @@
+package pubg
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type API struct {
+	APIKey  string
+	BaseURL *url.URL
+}
+
+type Player struct {
+	PlatformID     int       `json:"platformId"`
+	AccountID      string    `json:"AccountId"`
+	Avatar         string    `json:"Avatar"`
+	SelectedRegion string    `json:"selectedRegion"`
+	DefaultSeason  string    `json:"defaultSeason"`
+	SeasonDisplay  string    `json:"seasonDisplay"`
+	LastUpdated    time.Time `json:"LastUpdated"`
+	LiveTracking   []struct {
+		Match        int         `json:"Match"`
+		MatchDisplay string      `json:"MatchDisplay"`
+		Season       int         `json:"Season"`
+		RegionID     int         `json:"RegionId"`
+		Region       string      `json:"Region"`
+		Date         string      `json:"Date"`
+		Delta        float64     `json:"Delta"`
+		Value        float64     `json:"Value"`
+		Message      interface{} `json:"message"`
+	} `json:"LiveTracking"`
+	PlayerName    string `json:"PlayerName"`
+	PubgTrackerID int    `json:"PubgTrackerId"`
+	Stats         []struct {
+		Region string `json:"Region"`
+		Season string `json:"Season"`
+		Match  string `json:"Match"`
+		Stats  []struct {
+			Partition    interface{} `json:"partition"`
+			Label        string      `json:"label"`
+			SubLabel     interface{} `json:"subLabel"`
+			Field        string      `json:"field"`
+			Category     string      `json:"category"`
+			ValueInt     interface{} `json:"ValueInt"`
+			ValueDec     float64     `json:"ValueDec"`
+			Value        string      `json:"value"`
+			Rank         interface{} `json:"rank"`
+			Percentile   float64     `json:"percentile"`
+			DisplayValue string      `json:"displayValue"`
+		} `json:"Stats"`
+	} `json:"Stats"`
+	MatchHistory []struct {
+		Updated          string  `json:"Updated"`
+		UpdatedJS        string  `json:"UpdatedJS"`
+		Season           int     `json:"Season"`
+		SeasonDisplay    string  `json:"SeasonDisplay"`
+		Match            int     `json:"Match"`
+		MatchDisplay     string  `json:"MatchDisplay"`
+		Region           int     `json:"Region"`
+		RegionDisplay    string  `json:"RegionDisplay"`
+		Rounds           int     `json:"Rounds"`
+		Wins             int     `json:"Wins"`
+		Kills            int     `json:"Kills"`
+		Assists          int     `json:"Assists"`
+		Top10            int     `json:"Top10"`
+		Rating           float64 `json:"Rating"`
+		RatingChange     float64 `json:"RatingChange"`
+		RatingRank       int     `json:"RatingRank"`
+		RatingRankChange int     `json:"RatingRankChange"`
+		Kd               float64 `json:"Kd"`
+	} `json:"MatchHistory"`
+}
+
+func CreateAPI(key string) *API {
+	base, err := url.Parse("https://pubgtracker.com/api/profile/pc/")
+	if err != nil {
+		panic(err)
+	}
+
+	return &API{
+		APIKey:  key,
+		BaseURL: base,
+	}
+}
+
+func (a *API) NewRequest(endpoint string) *http.Request {
+	end, err := url.Parse(endpoint)
+	if err != nil {
+		panic(err)
+	}
+	urlStr := a.BaseURL.ResolveReference(end)
+
+	req, err := http.NewRequest("GET", urlStr.String(), nil)
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Add("trn-api-key", a.APIKey)
+
+	return req
+}
+
+func (a *API) Do(req *http.Request, i interface{}) {
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Decode response
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(body, &i)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Returns unfiltered JSON object
+func (a *API) GetPlayerByName(uname string) *Player {
+	req := a.NewRequest(uname)
+	a.NewRequest(uname)
+
+	var player Player
+	a.Do(req, &player)
+
+	return &player
+}
